9a140b9817cea06a51cbc1772f3a42fc4d58141f
Marinos Zakynthinos <marinoszak@gmail.com>
2020-02-23 23:48:01 +0200

    Styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import { Power3, TimelineLite } from 'gsap'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import List from './List'
 
 const Styling = styled.aside`
+  background-color: #fcfbf7;
+  opacity: 0;
+  visibility: hidden;
   position: fixed;
   width: 100%;
   z-index: 2;
   left: 0;
   bottom: 0;
   height: 10rem;
-  background: white;
 `
 
 const Sidebar = props => {
+  const wrapperRef = React.useRef()
+
+  useEffect(() => {
+    const timeline = new TimelineLite()
+    timeline.to(wrapperRef.current, 0.3, {
+      ease: Power3.easeInOut,
+      autoAlpha: 1,
+    })
+  }, [])
   return (
-    <Styling>
+    <Styling ref={wrapperRef}>
       <List {...props} />
     </Styling>
   )
